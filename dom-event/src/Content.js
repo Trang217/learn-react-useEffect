4297b53ca5dbb3412485ac1d9d9f2e5ad98bdd7a
Trang Nguyen <nguyenminhtrang9213@gmail.com>
2021-10-26 23:13:51 +0200

    Clean up for useEffect

--- dom-event/src/Content.js
+++ dom-event/src/Content.js
@@ -6,6 +6,9 @@ const Content = () => {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
   const [type, setType] = useState(tabs[0]);
+  const [showGoToTop, setShowGoToTop] = useState(false);
+
+  const [width, setWidth] = useState(window.innerWidth);
   // useEffect(callback, [dependency])
   useEffect(() => {
     // fetch(`https://jsonplaceholder.typicode.com/posts`)
@@ -16,8 +19,31 @@ const Content = () => {
       .then((posts) => setPosts(posts));
   }, [type]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowGoToTop(window.scrollY >= 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div>
+      <h1>{width}</h1>
       {tabs.map((tab) => (
         <button
           onClick={() => setType(tab)}
@@ -33,6 +59,12 @@ const Content = () => {
           <li key={post.id}>{post.title || post.name}</li>
         ))}
       </ul>
+
+      {showGoToTop && (
+        <button style={{ position: "fixed", right: 20, bottom: 20 }}>
+          Go to top
+        </button>
+      )}
     </div>
   );
 };
